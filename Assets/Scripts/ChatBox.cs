ab1f52051b7b8ce00d1c3e901c93590d45955993
mah01dis <mhdsqvd@gmail.com>
2025-02-28 14:36:26 +0330

    chat box

---
+++ Assets/Scripts/ChatBox.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChatBox : MonoBehaviour
+{
+    public TextTyper textTyper; // Reference to the TextTyper component
+    public string[] messages = new string[]
+        {
+            "Hello! Welcome to the chat.",
+            "How can I assist you today?",
+            "Feel free to ask any questions.",
+            "Have a great day!"
+        };  // Array of messages to display
+    private int currentMessageIndex = 0; // Index of the current message
+
+    private void Start()
+    {
+        
+        // Start typing the first message
+        textTyper.StartTyping(messages[currentMessageIndex]);
+    }
+
+    public void OnPointerClick()
+    {
+        // If there are more messages, show the next one
+        if (currentMessageIndex < messages.Length - 1)
+        {
+            currentMessageIndex++;
+            textTyper.StartTyping(messages[currentMessageIndex]);
+        }
+    }
+}

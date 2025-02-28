9259e806ad52928cb2d48d33deef1f879c60d57c
mah01dis <mhdsqvd@gmail.com>
2025-02-28 12:22:08 +0330

    aaa

--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -1,11 +1,23 @@
+using System.Collections;
 using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
+    public Car[] cars;
+    public bool spawn = true;
+    public float spawnTimer = 2;
+    public int index = 0;
+    public Transform spawnPoint;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    IEnumerator Start()
     {
-        
+        while (true)
+        {
+            while (spawn == false) yield return null;
+            Instantiate(cars[index % cars.Length - 1], spawnPoint);
+            yield return new WaitForSeconds(spawnTimer);
+        }
     }
 
     // Update is called once per frame
